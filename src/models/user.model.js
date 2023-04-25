7483198fb4b7760e02798f389a5600367c765d90
Heurtault Aubin <aub.heurtault@gmail.com>
2023-04-25 11:47:38 +0200

    create user models

---
+++ src/models/user.model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    firstName: String,
+    lastName: String,
+    gender: String,
+    age: Number,
+    height: Number,
+    weight: Number,
+    fitnessLevel: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
