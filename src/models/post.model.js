4cbb935cd57c959a64061049d69a2d49acddb98f
Heurtault Aubin <aub.heurtault@gmail.com>
2023-04-25 16:28:25 +0200

    add post model(

---
+++ src/models/post.model.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    comments: [commentSchema],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
