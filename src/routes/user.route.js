85905f29202115bce67e48998c071106c963e105
Heurtault Aubin <aub.heurtault@gmail.com>
2023-06-08 09:24:45 +0200

    fix swagger

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,14 +3,10 @@
  * tags:
  *   name: User
  *   description: User management
- *
- * @swagger
  * /user/follow/{userIdToFollow}:
  *   post:
  *     summary: Follow a user
  *     tags: [User]
- *     security:
- *       - BearerAuth: []
  *     parameters:
  *       - in: path
  *         name: userIdToFollow
@@ -27,14 +23,15 @@
  *         description: User to follow not found
  *       500:
  *         description: Internal server error
+ *     description: |
+ *       **Instructions d'authentification :**
  *
- * @swagger
+ *       Pour exécuter cette requête, incluez le token d'authentification dans l'en-tête "Authorization" de la requête. Le token doit être au format "Bearer {votre_token}". Assurez-vous d'avoir préalablement obtenu un token valide en effectuant une requête d'authentification.
+ *
  * /user/followers:
  *   get:
  *     summary: Get the list of followers for the currently authenticated user
  *     tags: [User]
- *     security:
- *       - BearerAuth: []
  *     responses:
  *       200:
  *         description: The list of followers for the currently authenticated user
@@ -48,16 +45,72 @@
  *         description: User not found
  *       500:
  *         description: Internal server error
+ *
+ * /user/delete:
+ *   delete:
+ *     summary: Delete the currently authenticated user account
+ *     tags: [User]
+ *     responses:
+ *       200:
+ *         description: User account successfully deleted
+ *       500:
+ *         description: Internal server error
+ *
+ * /user/update:
+ *   put:
+ *     summary: Update user information
+ *     tags: [User]
+ *     requestBody:
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *               gender:
+ *                 type: string
+ *               age:
+ *                 type: integer
+ *               height:
+ *                 type: number
+ *               weight:
+ *                 type: number
+ *               fitnessLevel:
+ *                 type: string
+ *               profileImage:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       200:
+ *         description: User information updated successfully
+ *       400:
+ *         description: Invalid request body
+ *       401:
+ *         description: Unauthorized request, token is missing or invalid
+ *       404:
+ *         description: User not found
+ *       500:
+ *         description: Internal server error
  */
 
 const express = require('express');
 const router = express.Router();
-const {getFollowers,followUser} = require('../controllers/user.controller')
-const authMiddleware = require('../middlewares/auth.middleware');
+const { getFollowers, followUser, deleteUser, updateUser } = require('../controllers/user.controller');
+const authMiddleware = require("../middlewares/auth.middleware");
+
 // Route pour suivre un utilisateur
 router.post('/follow/:userIdToFollow', authMiddleware, followUser);
 
 // Route pour récupérer les followers de l'utilisateur actuellement authentifié
 router.get('/followers', authMiddleware, getFollowers);
+
+// Route pour supprimer le compte utilisateur actuellement authentifié
+router.delete('/delete', authMiddleware, deleteUser);
+
+// Route pour mettre à jour les informations de l'utilisateur actuellement authentifié
+router.put('/update', authMiddleware, updateUser);
 
 module.exports = router;
