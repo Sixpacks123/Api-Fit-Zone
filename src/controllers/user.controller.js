dca184c7002b5c2f7c3385698fac4e668b1d4dd5
Heurtault Aubin <aub.heurtault@gmail.com>
2023-06-08 09:24:23 +0200

    config multer

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,20 @@
 const db = require('../models');
 const User = db.user;
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        // Specify the directory where uploaded files will be stored
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        // Generate a unique filename for the uploaded file
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, file.fieldname + '-' + uniqueSuffix);
+    }
+});
 
+// Create the multer upload instance
+const upload = multer({ storage: storage });
 exports.followUser = async (req, res) => {
     try {
         const { userIdToFollow } = req.params;
@@ -44,5 +58,53 @@ exports.getFollowers = async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Une erreur est survenue lors de la récupération des followers' });
+    }
+};
+
+exports.deleteUser = async (req, res) => {
+    try {
+        const userId = req.user._id;
+
+        // Supprime l'utilisateur de la base de données
+        await User.findByIdAndRemove(userId);
+
+        res.status(204).end();
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Une erreur est survenue lors de la suppression du compte utilisateur' });
+    }
+};
+exports.updateUser = async (req, res) => {
+    try {
+
+        // Vérifie si l'utilisateur existe
+        console.log(req.user.id)
+        const user = await User.findById(req.user.id);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        // Update the user information with the data provided in the request body
+        user.firstName = req.body.firstName;
+        user.lastName = req.body.lastName;
+        user.gender = req.body.gender;
+        user.age = req.body.age;
+        user.height = req.body.height;
+        user.weight = req.body.weight;
+        user.fitnessLevel = req.body.fitnessLevel;
+
+        // Check if a file was uploaded
+        if (req.file) {
+            // Update the user's profile image path
+            user.profileImage = req.file.path;
+        }
+
+        // Save the user changes to the database
+        await user.save();
+
+        res.status(200).json({ message: 'User information updated successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'An error occurred while updating user information' });
     }
 };
