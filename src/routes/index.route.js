be05f0fadb73c731fa5ba55398cdb46de1c9f5f6
Heurtault Aubin <aub.heurtault@gmail.com>
2023-06-08 09:23:31 +0200

    Add user routes

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -4,11 +4,13 @@ const auth = require('./auth.route');
 const program = require('./program.route')
 const exercise = require('./exercise.route');
 const post = require('./post.route');
+const user = require('./user.route');
 
 router.use('/program',program)
 router.use('/auth',auth)
 router.use('/exercise',exercise)
 router.use('/post',post)
+router.use('/user',user)
 router.get('/health', (req, res) => {
     const healthcheck = {
         uptime: process.uptime(),
